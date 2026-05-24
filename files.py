5c90b90ea8220d30a63d34cbf0f04e74087af736
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-24 10:34:58 +0930

    add list_page def for listing files

--- files.py
+++ files.py
@@ -19,6 +19,28 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         if self.path == "/upload":
             self.upload_file()
 
+    def list_page(self):
+        files = os.listdir(SHARED_FOLDER)
+        html = "<html><body>"
+        html += "<h2>Simple Files</h2>"
+        html += """
+        <form method="POST" action="/upload" enctype="multipart/form-data">
+            <input name="file" type="file" />
+            <input type="submit" value="Upload" />
+        </form>
+        <hr>
+        """
+        html == "<h3>Files</h3><ul>"
+        for f in files:
+            html += f'<li><a href="{f}">{f}</a></li>'
+
+        html += "</ul></body></html>"
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html.encode())
+
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 Handler = http.server.SimpleHTTPRequestHandler
