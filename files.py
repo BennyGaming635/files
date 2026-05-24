0115fa8b9dd4eca68cf35041b50b4a716b6086a1
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-05-24 10:26:00 +0930

    replace + with *

--- files.py
+++ files.py
@@ -14,13 +14,13 @@ local_ip = socket.gethostbyname(hostname)
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
-    print("=" + 40)
+    print("=" * 40)
     print("Files | The simple file server")
-    print("=" + 40)
+    print("=" * 40)
     print(f"Serving your simple files in: {os.getcwd()}")
     print()
     print("Open this on other devices:")
     print(f"http://{local_ip}:{PORT}")
-    print("=" + 40)
+    print("=" * 40)
 
     httpd.serve_forever()
